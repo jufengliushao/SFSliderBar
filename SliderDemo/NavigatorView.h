afc2463704a07e40a94d089887dc98b764364f35
刘少锋 <liushaofeng@cnlive.com>
2017-02-07 11:58:49 +0800

    add some method

--- SliderDemo/NavigatorView.h
+++ SliderDemo/NavigatorView.h
@@ -11,10 +11,60 @@
 typedef void(^SelectAction)(NSInteger index);
 
 @interface NavigatorView : UIView
+/**
+ 标题数组
+ */
 @property (nonatomic, strong) NSArray *titleArr;
-@property (nonatomic, copy) SelectAction selectAction;
-//@property (nonatomic, assign) CGFloat lineTargetX;
+/**
+ 标题正常颜色
+ */
+@property (nonatomic, strong) UIColor *textNormalColor;
+/**
+ 标题选中颜色
+ */
+@property (nonatomic, strong) UIColor *textSeletedColor;
+/**
+ 线的选中颜色
+ */
+@property (nonatomic, strong) UIColor *lineColor;
+/**
+ 标题正常字体大小
+ */
+@property (nonatomic, strong) UIFont *textNormalFont;
+/**
+ 标题选中字体大小
+ */
+@property (nonatomic, strong) UIFont *textSeletedFont;
+/**
+ 下划线的高度
+ */
+@property (nonatomic, assign) CGFloat lineViewHeight;
+/**
+ 每个item之间的间距 （不包括最左边和左右边）
+ */
+@property (nonatomic, assign) CGFloat itemSpace;
+/**
+ 最左边item距离边距的大小
+ */
+@property (nonatomic, assign) CGFloat itemLeftSpace;
 
+/**
+ index点击事件
+ */
+@property (nonatomic, copy) SelectAction selectAction;
+
+
+/**
+ scrollerView滑动 更改下划线位置
+ 
+ @param rate 滑动比例
+ */
 - (void)scrollerDidScrollerWithRate:(CGFloat)rate;
+
+/**
+ 滑动页面重新选中item
+ 
+ @param index 选中的item index
+ */
 - (void)resetSelectedIndexWithIndex:(NSInteger)index;
 @end
--- SliderDemo/NavigatorView.m
+++ SliderDemo/NavigatorView.m
@@ -15,7 +15,12 @@
     UIFont *_selectedFont;
     UIColor *_normalColor;
     UIColor *_selectedColor;
-    CGFloat _oldLineX;
+    UIColor *_lineViewColor;
+    CGFloat _lineHeight;
+    CGFloat _btnSpace;
+    CGFloat _btnLeftSpace;
+    
+    CGFloat _totalWidth;
 }
 
 @end
@@ -30,16 +35,59 @@ static long long itemTagBase = 11000;
         _selectedFont = [UIFont systemFontOfSize:16];
         _normalColor = [UIColor whiteColor];
         _selectedColor = [UIColor orangeColor];
+        _lineViewColor = [UIColor orangeColor];
         _lineView = [[UIView alloc] init];
-        _lineView.backgroundColor = _selectedColor;
+        _lineView.backgroundColor = _lineViewColor;
+        _lineHeight = 2.0;
+        _btnSpace = 20;
+        _btnLeftSpace = 20;
+        _totalWidth = 0;
     }
     return self;
 }
 
+- (void)drawRect:(CGRect)rect {
+    [self addBtn];
+    [self resetItemsProperty];
+}
+
+#pragma mark - setter
 - (void)setTitleArr:(NSArray *)titleArr{
     _titleArr = titleArr;
     [self creatItems];
     [self addBtn];
+}
+
+- (void)setTextNormalFont:(UIFont *)textNormalFont{
+    _normalFont = textNormalFont;
+}
+
+- (void)setTextSeletedFont:(UIFont *)textSeletedFont{
+    _selectedFont = textSeletedFont;
+}
+
+- (void)setTextNormalColor:(UIColor *)textNormalColor{
+    _normalColor = textNormalColor;
+}
+
+- (void)setTextSeletedColor:(UIColor *)textSeletedColor{
+    _selectedColor = textSeletedColor;
+}
+
+- (void)setLineColor:(UIColor *)lineColor{
+    _lineViewColor = lineColor;
+}
+
+- (void)setLineViewHeight:(CGFloat)lineViewHeight{
+    _lineHeight = lineViewHeight;
+}
+
+- (void)setItemSpace:(CGFloat)itemSpace{
+    _btnSpace = itemSpace;
+}
+
+- (void)setItemLeftSpace:(CGFloat)itemLeftSpace{
+    _btnLeftSpace = itemLeftSpace;
 }
 
 #pragma mark - method
@@ -59,33 +107,57 @@ static long long itemTagBase = 11000;
     }
 }
 
+- (void)resetItemsProperty{
+    for (UIButton *btn in _btnArr) {
+        [btn setTitleColor:_normalColor forState:(UIControlStateNormal)];
+        [btn setTitleColor:_selectedColor forState:(UIControlStateSelected)];
+        btn.titleLabel.font = _normalFont;
+        btn.backgroundColor = [UIColor clearColor];
+    }
+    _lineView.backgroundColor = _lineViewColor;
+}
+
 - (void)addBtn{
     NSInteger index = 0;
     for (UIButton *btn in _btnArr) {
-        btn.frame = CGRectMake(80 * index, 0, 80, self.bounds.size.height - 2);
+        CGFloat width = [self returnTitleWidthWithTitle:[btn titleForState:(UIControlStateNormal)]];
+        btn.frame = CGRectMake((width + _btnSpace) * index + _btnLeftSpace, 0, width, self.bounds.size.height - _lineHeight);
         if (index == 0) {
             btn.selected = YES;
-            _lineView.frame = CGRectMake(0, btn.frame.origin.y + btn.bounds.size.height, btn.bounds.size.width, 2);
-            [self addSubview:_lineView];
+            _lineView.frame = CGRectMake(_btnLeftSpace, btn.frame.origin.y + btn.bounds.size.height, btn.bounds.size.width, _lineHeight);
+            if (![self.subviews containsObject:_lineView]) {
+                [self addSubview:_lineView];
+            }
         }
-        [self addSubview:btn];
+        if (![self.subviews containsObject:btn]) {
+            [self addSubview:btn];
+        }
         index += 1;
     }
+    UIButton *btn = [_btnArr lastObject];
+    _totalWidth = btn.frame.origin.x + btn.bounds.size.width;
 }
 
 - (void)setLineTargetX:(CGFloat)lineTargetX{
-    [self changeLineViewFrameWithX:lineTargetX];
+    [self changeLineViewFrameWithX:lineTargetX lineWidth:_lineView.bounds.size.width];
 }
 
 - (void)scrollerDidScrollerWithRate:(CGFloat)rate{
-    CGFloat targetX = rate * self.bounds.size.width;
-    [self changeLineViewFrameWithX:targetX];
+    CGFloat targetX = rate * _totalWidth;
+    [self changeLineViewFrameWithX:targetX + _btnLeftSpace lineWidth:_lineView.bounds.size.width];
+}
+
+- (CGFloat)returnTitleWidthWithTitle:(NSString *)title{
+    CGFloat itemWidth = 0.0f;
+    NSDictionary *dic = [NSDictionary dictionaryWithObject:_selectedFont forKey:NSFontAttributeName];
+    itemWidth = [title boundingRectWithSize:CGSizeMake(0, self.bounds.size.height - _lineHeight) options:(NSStringDrawingUsesLineFragmentOrigin) attributes:dic context:nil].size.width;
+    return itemWidth;
 }
 #pragma mark - action
 - (void)itemClickAction:(UIButton *)btn{
     [self resetSelectedIndexWithIndex:btn.tag - itemTagBase];
     CGFloat target = [_btnArr[btn.tag - itemTagBase] frame].origin.x;
-    [self animationChangeLineViewFrameWithTargetX:target];
+    [self animationChangeLineViewFrameWithTargetX:target lineWith:[_btnArr[btn.tag - itemTagBase] frame].size.width];
     if (self.selectAction) {
         self.selectAction(btn.tag - itemTagBase);
     }
@@ -100,24 +172,21 @@ static long long itemTagBase = 11000;
     }
 }
 
-- (void)changeLineViewFrameWithX:(CGFloat)targetX{
-    _lineView.frame = CGRectMake(targetX, _lineView.frame.origin.y, _lineView.bounds.size.width, _lineView.bounds.size.height);
+- (void)changeLineViewFrameWithX:(CGFloat)targetX lineWidth:(CGFloat)width{
+    _lineView.frame = CGRectMake(targetX, _lineView.frame.origin.y, width, _lineView.bounds.size.height);
 }
 
-- (void)animationChangeLineViewFrameWithTargetX:(CGFloat)targetX{
+- (void)animationChangeLineViewFrameWithTargetX:(CGFloat)targetX lineWith:(CGFloat)width{
     [UIView animateWithDuration:0.5 animations:^{
-        [self changeLineViewFrameWithX:targetX];
+        [self changeLineViewFrameWithX:targetX+_btnLeftSpace lineWidth:width];
     }];
 }
 
 #pragma mark - init
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ */
 
 @end
